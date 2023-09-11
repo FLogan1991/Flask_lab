8b0fe4bcdb93b585252f706e25a0f2b62e63ced1
Logan Furlong <furlong_logan@columbusstate.edu>
2023-09-11 02:30:45 +0000

    added extenders for the templates files

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
@@ -10,3 +10,8 @@ if __name__ == "__main__":
 @app.route("/<name>")
 def hello_name(name):
     return "Hello " + name
+
+@app.route("/about")
+def about():
+    name = request.args.get('name') if request.args.get('name') else "Hello World!" 
+    return render_template("about.html", aboutName=name) 
