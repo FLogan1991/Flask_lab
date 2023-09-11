cae60fc5a0846cdb95d532aa1f4acf1087ceb994
Logan Furlong <furlong_logan@columbusstate.edu>
2023-09-11 01:32:48 +0000

    Corrected templates folder name from template

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-
 app = Flask(__name__)
 
 @app.route("/")
@@ -11,4 +10,3 @@ if __name__ == "__main__":
 @app.route("/<name>")
 def hello_name(name):
     return "Hello " + name
-
