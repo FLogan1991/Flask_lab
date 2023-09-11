02c86e23eaae699e9829c9101a9df8449330ca00
Logan Furlong <furlong_logan@columbusstate.edu>
2023-09-11 03:11:50 +0000

    Added API to the home screen only.

--- app.py
+++ app.py
@@ -2,9 +2,17 @@ from flask import Flask, render_template, request
 import urllib.request, json
 
 app = Flask(__name__)
+# We need to add the URL we will be using to fetch information from.
+# Sometimes this means also sending some data like a key, but not for this one
+url = "https://xkcd.com/info.0.json"
+response = urllib.request.urlopen(url)
+# Once we have the response we need to extract the data we want.
+data = response.read()
+dict = json.loads(data)
 
 @app.route("/")
 def hello():
+    #only added the api to the home page to make the website look more clean
     return render_template("index.html", datum=dict)
 
 @app.route("/<name>")
@@ -17,11 +25,4 @@ def about():
     return render_template("about.html", aboutName=name) 
 
 if __name__ == "__main__":
-    # We need to add the URL we will be using to fetch information from.
-    # Sometimes this means also sending some data like a key, but not for this one
-    url = "https://xkcd.com/info.0.json"
-    response = urllib.request.urlopen(url)
-    # Once we have the response we need to extract the data we want.
-    data = response.read()
-    dict = json.loads(data)
     app.run(debug=True)
