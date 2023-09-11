ad4d251eee5621c8cb2372bfcb471956f15f88b0
Logan Furlong <furlong_logan@columbusstate.edu>
2023-09-11 01:26:40 +0000

    No changes made

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ def hello():
     return render_template("index.html")
 if __name__ == "__main__":
     app.run(debug=True)
-    
+
 @app.route("/<name>")
 def hello_name(name):
     return "Hello " + name
+
