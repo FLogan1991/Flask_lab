c7a3765fd8b130fab592b955e6b4df07709f62c4
Logan Furlong <furlong_logan@columbusstate.edu>
2023-09-11 02:53:04 +0000

    added API integration in the home page

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request
+import urllib.request, json
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template("index.html")
+    return render_template("index.html", datum=dict)
 
 @app.route("/<name>")
 def hello_name(name):
@@ -15,4 +17,11 @@ def about():
     return render_template("about.html", aboutName=name) 
 
 if __name__ == "__main__":
+    # We need to add the URL we will be using to fetch information from.
+    # Sometimes this means also sending some data like a key, but not for this one
+    url = "https://xkcd.com/info.0.json"
+    response = urllib.request.urlopen(url)
+    # Once we have the response we need to extract the data we want.
+    data = response.read()
+    dict = json.loads(data)
     app.run(debug=True)
